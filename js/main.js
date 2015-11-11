b511cfe01ec6ae61ba9d2ff8e64e2430f57e9117
Владислав <vladis1ove81@gmail.com>
2015-11-12 03:55:58 +0600

    no message

--- js/main.js
+++ js/main.js
@@ -47,9 +47,65 @@ $(document).ready(function(){
         infinite: true,
         fade: true,
         arrows: false,
-        autoplaySpeed: 3000
+        autoplaySpeed: 3000,
+        draggable: false
     });
 
+    $('.slick-slider2').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        arrows: false,
+        autoplaySpeed: 3000,
+        draggable: false
+    });
+    $('.slick-slider2').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $(this).find(".person-photo").fadeOut(200);
+    });
+    $('.slick-slider2').on('afterChange', function(event, slick, currentSlide){
+        $(this).find(".person-photo").fadeIn();
+    });
+
+    var open,active,animate = true;
+    $(".b-city-name").click(function(){
+        if(!$(this).parent().hasClass("active")) {
+            animate = false;
+            var obj = $(this).parent();
+            obj.addClass("active");
+            obj.find(".b-cloud3").fadeIn(200,function(){
+                obj.find(".b-cloud2").fadeIn(200,function(){
+                    obj.find(".b-cloud-cont").fadeIn();
+                    animate = true;
+                });
+            });
+        }
+        
+    });
+    
+    function closeBubble(active){
+        if( typeof active == "undefined" ) active = $(".b-city.active");
+            active.removeClass('active');
+            active.find('.b-cloud3').hide();
+            active.find('.b-cloud2').hide();
+            active.find('.b-cloud-cont').hide();
+    }
+
+        $("body").on("mouseup",".b-city.active,.b-cloud",function(){
+            open = true;
+        });
+
+        $("body").on("mousedown",function() {
+            open = false;
+        }).bind("mouseup",function(){
+            if( !open && animate)
+                closeBubble();
+        });
+        customHandlers['question_hide'] = function() {
+            $(".b-popup .b-popup-cont img").hide();
+        };
+        customHandlers['question_show'] = function() {
+            $(".b-popup .b-popup-cont img").fadeIn();
+        };
 	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
  //    var myOptions = {
  //        zoom: 16,
